07649e52d2b866e509d93fb1cae006b1286822cf
Longlee218 <longlehoang2013@gmail.com>
2022-08-20 01:29:56 +0700

    draw

--- src/pages/Teacher/Slide/Slide.jsx
+++ src/pages/Teacher/Slide/Slide.jsx
@@ -5,11 +5,24 @@ import React from 'react';
 
 const Slide = () => {
   return (
-    <div className='excalidraw-wrapper' style={{ height: '100vh' }}>
+    <div
+      className='excalidraw-wrapper'
+      style={{ height: 'calc(100vh - 60px)' }}
+    >
       <Excalidraw
         onChange={(elements, state) =>
           console.log('Elements :', elements, 'State : ', state)
         }
+        initialData={{
+          appState: {
+            viewBackgroundColor: '#f7f7f7',
+            currentItemFontFamily: 3,
+          },
+          scrollToContent: true,
+        }}
+        langCode='vi-VN'
+        // gridModeEnabled
+        s
         onPointerUpdate={(payload) => console.log(payload)}
         onCollabButtonClick={() => window.alert('You clicked on collab button')}
       />
